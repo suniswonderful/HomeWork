b0ac803ed171f0dff44cd0fd54b1ecfca5d87019
suniswonderful <suniswonderful@mail.ru>
2020-10-31 21:55:39 +0300

    delete

--- 21.py
+++
@@ -1,3 +0,0 @@
-spisok = ['Hello', 1, 33.2, True, 'World']
-for el in spisok:
-    print(type(el))
--- 24.py
+++
@@ -1,5 +0,0 @@
-elements_list = input('Enter your words  with spaces: ').split()
-for i in elements_list:
-    if len(i) > 10:
-        print(i[0:9])
-    else: print(i)
